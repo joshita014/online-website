d93a1a30dca2667662ef374e1360df360bfc3886
sandeep <sandeep.d@simplilearn.net>
2023-08-17 21:28:46 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* render the app component */}
+    <App />
+  </React.StrictMode>
+);
+{/* <div class='animated_gradient'></div> */}
+
+
+
